752f9cb426ea374be16a9ee1b382a6bee3cb7d30
boogie <wadhah.mahroug15@gmail.com>
2020-03-17 10:47:40 +0100

    init

---
+++ src/main/java/wassalni/BackEndCovoiturageApplication.java
@@ -0,0 +1,13 @@
+package wassalni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCovoiturageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndCovoiturageApplication.class, args);
+    }
+
+}
