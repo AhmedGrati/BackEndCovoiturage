53b12c4a5d0ede854cc85399853dd1a12dafb250
boogie <wadhah.mahroug15@gmail.com>
2020-03-17 23:27:30 +0100

    fix

--- src/main/java/wassalni/appUser/AppUserRepo.java
+++ src/main/java/wassalni/appUser/AppUserRepo.java
@@ -1,6 +1,8 @@
 package wassalni.appUser;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface AppUserRepo extends PagingAndSortingRepository<AppUserRepo, Long> {
+@RepositoryRestResource(collectionResourceRel = "Appuser", path = "appuser")
+public interface AppUserRepo extends PagingAndSortingRepository<AppUser, Long> {
 }
