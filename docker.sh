f97d50e0bab411acf6bdf5c5e2e46496ea1c5669
wadhah101 <wadhah.mahroug15@gmail.com>
2020-03-24 16:14:49 +0100

    docker integration

--- docker.sh
+++ docker.sh
@@ -1,2 +1,4 @@
-sed -i 's/127.0.0.1/mariadb/g'   src/main/resources/application.properties
-mvn  spring-boot:run
+sed -i 's/localhost/mariadb/g'   src/main/resources/application.properties
+mvn package
+sed -i 's/mariadb/localhost/g'   src/main/resources/application.properties
+java -jar target/*.jar
