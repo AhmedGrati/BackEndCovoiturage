1f21a103b0956dbffa8da6f793ace2bacbfff44b
wadhah <wadhah.mahroug15@gmail.com>
2020-05-31 20:31:32 +0100

    fix conflict

--- src/test/java/BackEndCovoiturage/Controller/UserControllerTest.java
+++ src/test/java/BackEndCovoiturage/Controller/UserControllerTest.java
@@ -2,7 +2,6 @@ package BackEndCovoiturage.Controller;
 
 import BackEndCovoiturage.Model.User;
 import BackEndCovoiturage.Service.UserService;
-import org.apache.commons.io.IOUtils;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,25 +10,21 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 public class UserControllerTest {
 
-    public final String storageDirectoryPathOnLinux = "/home/ubuntu/images";
 
 
     MockMvc mockMvc;
@@ -53,7 +48,7 @@ public class UserControllerTest {
                 new User() , new User() , new User()
         ).collect(Collectors.toList()));
         mockMvc.perform(get("/api/user/allUsers"))
-                    .andExpect(status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$",hasSize(3)));
 
@@ -110,55 +105,6 @@ public class UserControllerTest {
         verify(userService).sendEmail(receiverMail);
     }
 
-<<<<<<< HEAD
-    @Test
-    public void downloadImagePositiveTest() throws Exception {
-
-        String imageName = "0.jpg";
-        when(userService.getImageWithMediaType(imageName)).thenReturn(
-                IOUtils.toByteArray(Paths.get(storageDirectoryPathOnLinux + "\\" + imageName).toUri()));
-        mockMvc.perform(get("/api/user/images/getImage/" + imageName))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.IMAGE_JPEG));
-
-        verify(userService).getImageWithMediaType(imageName);
-
-
-    }
-
-    @Test
-    public void downloadImageNegativeTest() throws Exception {
-
-        String imageName = "jheyehehe.png";
-        String storageDirectoryPath = storageDirectoryPathOnLinux;
-        when(userService.getImageWithMediaType(imageName)).thenReturn(null);
-        mockMvc.perform(get("/api/user/images/getImage/"+imageName))
-                .andExpect(status().isOk());
-
-        verify(userService).getImageWithMediaType(imageName);
-    }
-
-    @Test
-    public void registerUserByIdNegativeTest() throws Exception{
-        String json = "{\n" +
-                "  \"email\" : \"ahmed@gmail.com\",\n" +
-                "  \"password\": \"ahmed\",\n" +
-                "  \"gender\" : \"male\"\n" +
-                "}";
-
-        byte[] content = Files.readAllBytes(Paths.get(storageDirectoryPathOnLinux));
-        when(userService.uploadToLocalFileSystem( new MockMultipartFile("0.jpg","0.jpg","image/jpeg",content)  , new User()))
-                .thenReturn("localhost:90/api/user/images/getImage/wad.png");
-
-        mockMvc.perform(post("/api/user/upload?file=0.jpg")
-                .content(json).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());// because the file form data is missing in the body request
-
-
-        verifyNoInteractions(userService);// because when the file is null we don't call the userService so there's no interactions with the userService
-    }
-
-=======
 //    @Test
 //    public void downloadImagePositiveTest() throws Exception {
 //
@@ -207,7 +153,6 @@ public class UserControllerTest {
 //        verifyNoInteractions(userService);// because when the file is null we don't call the userService so there's no interactions with the userService
 //    }
 //
->>>>>>> 44ae43e33586bd233b21c36ff9df45041ee82d46
 
 
 }
