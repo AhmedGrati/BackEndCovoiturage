29ee812235ce0c47c2eba2af163720bbdc345872
wadhah <wadhah@gmail.com>
2020-06-07 15:11:32 +0100

    added swagger to base path changed reset url

---
+++ src/main/java/BackEndCovoiturage/Controller/MainController.java
@@ -0,0 +1,15 @@
+package BackEndCovoiturage.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@CrossOrigin
+public class MainController {
+    @GetMapping("")
+    public RedirectView redirectToDocs(){
+        return new RedirectView("/swagger-ui/index.html?configUrl=%2Fapi-docs%2Fswagger-config");
+    }
+}
